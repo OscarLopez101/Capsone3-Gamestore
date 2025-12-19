bf1494ca8d43a0a15476321934dce6ba27a67dfe
Student <oscar.lopez102004.ol@gmail.com>
2025-12-18 18:50:21 -0800

    updated the shopping cart dao file

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -2,8 +2,10 @@ package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
 
-public interface ShoppingCartDao
-{
+public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+
+    void addProduct(int userId, int productId);          // POST behavior (insert or +1)
+    void updateQuantity(int userId, int productId, int quantity); // PUT behavior
+    void clearCart(int userId);                          // DELETE behavior
 }
